ca674a95277e38de6c210bf82e2ff12d5533448f
coriandar <88608040+coriandar@users.noreply.github.com>
2023-02-28 14:19:29 +1300

    Added CRUD

--- app.js
+++ app.js
@@ -20,13 +20,57 @@ app.set('views', path.join(__dirname, 'views'));
 /** setup Schemas */
 const Campground = require('./models/campground.js');
 
-/** test */
+/** method override, allow put, patch */
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+/** parse */
+app.use(express.urlencoded ({extended: true}));
+
+/** routes */
 app.get('/', (req, res) => {
     res.render('home.ejs');
 })
 
-// app.get('/make', async (req, res) => {
-//     const camp = new Campground( { title: 'beach', description: 'good'});
-//     await camp.save();
-//     res.send(camp);
-// });
+// all
+app.get('/campground', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campground/index.ejs', { campgrounds });
+})
+
+// create
+app.get('/campground/new', (req, res) => {
+    res.render('campground/new.ejs');
+})
+
+// post
+app.post('/campground', async (req, res) => {
+    const camp = new Campground(req.body.campground);
+    await camp.save();
+    res.redirect(`/campground/${camp._id}`);
+})
+
+// by :id
+app.get('/campground/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campground/show.ejs', { campground });
+})
+
+// edit
+app.get('/campground/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campground/edit.ejs', { campground });
+})
+
+app.put('/campground/:id', async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campground/${camp._id}`);
+})
+
+// delete
+app.delete('/campground/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campground');
+})
